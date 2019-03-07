537d24db350c241e2146e83bf53fca469fc917ed
蓝莓小果冻 <2328712329@qq.com>
2019-03-07 15:46:34 +0000

    demo_test

---
+++ MAIN/main.js
@@ -0,0 +1,8 @@
+const http = require('http'); 
+const os = require('os');
+const os.type(hsg)
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("" + hsg);
+});
