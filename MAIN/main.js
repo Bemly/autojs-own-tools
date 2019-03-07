9ce64ddc5d38636d7b8d487f3c5b23983c94c200
蓝莓小果冻 <2328712329@qq.com>
2019-03-07 23:51:02 +0800

    更新 main.js

--- MAIN/main.js
+++ MAIN/main.js
@@ -1,6 +1,6 @@
 const http = require('http'); 
 const os = require('os');
-const os.type(hsg)
+const os.type(hsg);
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
